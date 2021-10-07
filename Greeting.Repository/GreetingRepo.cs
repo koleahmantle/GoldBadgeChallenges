e4df4e64f09036d0e265472a0c6cd4a9ab3996e6
Koleah Mantle <koleahmantle@gmail.com>
2021-10-07 18:03:44 -0400

    Cafe Complete, Greeting object completed

--- Greeting.Repository/GreetingRepo.cs
+++ Greeting.Repository/GreetingRepo.cs
@@ -9,15 +9,46 @@ namespace Greeting.Repository
     class GreetingRepo
     {
         // Make private repo list
-
+        private List<PersonContent> _listOfGreetingContent = new List<PersonContent>();
         //Create
-
+        public void AddPersonContent(PersonContent person)
+        {
+            _listOfGreetingContent.Add(person);
+        }
         //Read
-
+        public List<PersonContent> DisplayGreetingContent()
+        {
+            return _listOfGreetingContent;
+        }
         //Update
 
         //Delete
-
+        public bool DeletePerson(int id )
+        {
+            PersonContent person = GetPersonContentById(id);
+            if(id == null)
+            {
+                return false;
+            }
+            int initialCount = _listOfGreetingContent.Count;
+            _listOfGreetingContent.Remove(person);
+            if(initialCount > _listOfGreetingContent.Count)
+            {
+                return true;
+            }
+            return false;
+        }
         //Helper Method 
+        public PersonContent GetPersonContentById(int id)
+        {
+            foreach(PersonContent person in _listOfGreetingContent)
+            {
+                if(person.Id == id)
+                {
+                    return person;
+                }
+            }
+            return null;
+        }
     }
 }
--- Greeting.Repository/PersonContent.cs
+++ Greeting.Repository/PersonContent.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace Greeting.Repository
 {
-    public class GreetingContent
+    public enum CustomerType
+    {
+        Potential = 1,
+        Current,
+        Past
+    }
+    public class PersonContent
     {
         //Make Person Object, properties: First Name, Last Name, Person Type
-
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public CustomerType Type { get; set; }
+        public int Id { get; set; }
         //Make constructures 
+        PersonContent() { }
+        PersonContent(string firstName, string lastName, CustomerType type, int id)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Type = type;
+            Id = id;
+        }
     }
 }
